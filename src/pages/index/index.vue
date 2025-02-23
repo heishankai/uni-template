25f84ca0565ad6f543947b074bdd3bceb3b38200
kli59 <kli59@mail.yst.com.cn>
2025-02-23 16:32:43 +0800

    faet: 安装uin-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,9 @@
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
+    <uni-card>
+      <text>这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
+    </uni-card>
     <!-- <custom-tab-bar /> -->
   </view>
 </template>
