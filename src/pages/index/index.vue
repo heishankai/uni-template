eab84132011e10b07bf5eff08af6633862064d48
kli59 <kli59@mail.yst.com.cn>
2025-03-15 17:10:27 +0800

    feat: 删除无用代码

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,5 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png" />
     <view class="text-area">
       <text class="title">{{ title }}</text>
     </view>
@@ -42,14 +41,6 @@ const handleClick = (value): void => {
   justify-content: center;
 }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
 
 .text-area {
   display: flex;
